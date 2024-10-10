a4aeccff6bb616542c6280079172d24093ec8648
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-10-09 20:30:53 -0400

    Added network setup with terraform

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "The AWS region to create resources in."
+  type        = string
+}
+
+variable "profile" {
+  description = "The AWS profile to use."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use."
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
