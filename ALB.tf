0c53052a1fa6e72ad17bc13121f0ed13ba4eabaf
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-12-01 18:30:21 -0500

    Updated ALB for HTTPS Listener

--- ALB.tf
+++ ALB.tf
@@ -25,10 +25,12 @@ resource "aws_lb_target_group" "webapp" {
   }
 }
 
-resource "aws_lb_listener" "http_listener" {
+resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.webapp_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.environment == "dev" ? aws_acm_certificate_validation.dev_ssl_cert_validation.certificate_arn : var.demo_certificate_arn
 
   default_action {
     type             = "forward"
