9a227d0ca3eb950e86c213b22510b032ba1e8b9b
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-12-01 18:29:06 -0500

    Update Lambda function for SMS key

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "EmailVerificationFunction" {
-  filename      = "D:/Semester - 3/CSYE 6225 - Assignments/lambdafunction.zip" # Package containing your Lambda code
+  filename      = "D:/Semester - 3/CSYE 6225 - Assignments/Lambda.zip" # Package containing your Lambda code
   function_name = "EmailVerificationFunction"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "index.handler"
@@ -7,9 +7,8 @@ resource "aws_lambda_function" "EmailVerificationFunction" {
 
   environment {
     variables = {
-      SENDGRID_API_KEY    = var.SENDGRID_API_KEY
-      APP_BASE_URL        = var.APP_BASE_URL
-      SENDGRID_FROM_EMAIL = var.SENDGRID_FROM_EMAIL
+      APP_BASE_URL = var.APP_BASE_URL
+      emailCreds   = var.emailCreds
     }
   }
 }
