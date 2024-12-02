57f4fb21f3fc3d9807681cf496c1c8a0fd5d0257
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-12-01 21:58:33 -0500

    Update ec2 device and ALB Sg

--- ec2-LT.tf
+++ ec2-LT.tf
@@ -15,7 +15,7 @@ resource "aws_launch_template" "app" {
   }
 
   block_device_mappings {
-    device_name = "/dev/xvda"
+    device_name = "/dev/sda1"
     ebs {
       volume_type           = "gp2"
       volume_size           = 25
